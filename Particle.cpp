e6dfb857213264adacd58713fc7414253059fd34
miromeeeter <micromeeeter@gmail.com>
2018-06-17 13:50:48 +0900

    fix parameter and so on

--- Particle.cpp
+++ Particle.cpp
@@ -16,39 +16,51 @@ void Particle :: setup(int w, int h){
     
     for(int i = 0; i < MAX; i++){
         pos[i].set(ofRandom(0.0, 1.0), ofRandom(0.0, 1.0));
+        init_x[i] = pos[i].x;
     }
     
     vbo.setVertexData(drawPos, MAX, GL_DYNAMIC_DRAW);
     
     //general
     particleNum = 0;    //0 - MAX
-    modulation = 0.;
+    lfo = 0.;
+    speed = 0.1;
     range_x = 1.0;
     range_y = 1.0;
     size = 0.5;
-    mode = BROWNIAN;
-    //gathering
-    attractor_size = 0.5;
+    mode = BROWN;
+    
+    //bubble
+    for(int i = 0; i < MAX; i++){
+        bubble_range[i] = ofRandom(0.01, 0.005);
+    }
+    theta = 0.0;
+    
+    //gravity
     attractor_x = 0.5;
     attractor_y = 0.5;
+    
 }
 
 void Particle :: update(){
+    if(particleNum == 0 || mute == true){
+        return;
+    }
     
     //pos & vel are defined as 0-1
     if(particleNum > 0){
         switch (mode)
         {
-            case BROWNIAN:
-                blownianMotion();
+            case BROWN:
+                brownMotion();
+                break;
+                
+            case BUBBLE:
+                bubbleMotion();
                 break;
                 
             case GRAVITY:
                 gravityMotion();
-                break;
-                
-            case GATHERING:
-                gatheringMotion();
                 break;
                 
             default:
@@ -56,7 +68,7 @@ void Particle :: update(){
         }
         
         for(int i = 0; i < MAX; i++){
-            pos[i] += vel[i];
+            //            pos[i] += vel[i];
             
             //boundary condition
             if(pos[i].x < 0.0){
@@ -91,7 +103,7 @@ void Particle :: update(){
 
 void Particle :: draw(){
     
-    if(particleNum == 0){
+    if(particleNum == 0 || mute == true){
         return;
     }
     
@@ -105,10 +117,41 @@ void Particle :: draw(){
     
 }
 
-void Particle :: blownianMotion(){
+void Particle :: brownMotion(){
+    float coef = lfo * (speed + 1.0) * BROWN_COEF;
     
     for(int i = 0; i < MAX; i++){
-        vel[i].set(modulation * MOD_COEF * ofRandom(-1.0, 1.0), modulation * MOD_COEF * ofRandom(-1.0, 1.0));
+        vel[i].set(coef * ofRandom(-1.0, 1.0), coef * ofRandom(-1.0, 1.0));
+        pos[i] += vel[i];
+        
+    }
+    
+}
+
+void Particle :: bubbleInit(){
+    for(int i = 0; i < MAX; i++){
+        init_x[i] = pos[i].x;
+        vel[i].y = 0.0;
+    }
+}
+
+void Particle :: bubbleMotion(){
+    theta += speed * 0.1;
+    if(theta > 360.0){
+        theta = 0.0;
+    }
+    
+    for(int i = 0; i < MAX; i++){
+        if(vel[i].y <= BUBBLE_SPD_LIMIT * (speed + 1.0)){
+            vel[i].y = BUBBLE_SPD_LIMIT * (speed + 1.0);
+        }else{
+            vel[i].y += BUBBLE_SPD_ACC * (speed + 1.0);
+        }
+        
+        float diff = lfo * bubble_range[i] * sin(theta + i);
+        
+        pos[i].x = init_x[i] + diff;
+        pos[i].y += vel[i].y;
     }
     
 }
@@ -116,30 +159,15 @@ void Particle :: blownianMotion(){
 void Particle :: gravityMotion(){
     
     for(int i = 0; i < MAX; i++){
-        float next_y;
-        if(vel[i].y <= GRAVITY_SPD_LIMIT){
-            vel[i].y = GRAVITY_SPD_LIMIT;
-            next_y = vel[i].y;
-        }else{
-            
-            next_y = vel[i].y + GRAVITY_SPD_ACC;
-        }
-        
-        vel[i].set(modulation * MOD_COEF * ofRandom(-1.0, 1.0), next_y);
-    }
-    
-}
-
-void Particle :: gatheringMotion(){
-    
-    for(int i = 0; i < MAX; i++){
         float theta = atan2(drawPos[i].y - attractor_y, drawPos[i].x - attractor_x);
         float dist = ofDist(drawPos[i].x, drawPos[i].y, attractor_x, attractor_y);
-        if(dist < 100.0 && vel[i].length() > GATHERING_SPD_LIMIT){
-            vel[i] = vel[i].normalize() * GATHERING_SPD_LIMIT;
+        if(dist < 100.0 && vel[i].length() > GRAVITY_SPD_LIMIT){
+            vel[i] = vel[i].normalize() * GRAVITY_SPD_LIMIT;
         }
-        vel[i].x += ATTRACTOR_A * attractor_size * cos(theta) + modulation * MOD_COEF * ofRandom(-1.0, 1.0);
-        vel[i].y += ATTRACTOR_A * attractor_size * sin(theta) + modulation * MOD_COEF * ofRandom(-1.0, 1.0);
+        vel[i].x += ATTRACTOR_A * speed * cos(theta) + lfo * MOD_COEF * ofRandom(-1.0, 1.0);
+        vel[i].y += ATTRACTOR_A * speed * sin(theta) + lfo * MOD_COEF * ofRandom(-1.0, 1.0);
+        
+        pos[i] += vel[i];
     }
     
 }
